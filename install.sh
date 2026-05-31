3df71b1cd3c7b28c2a6c511431a2f8007e4b2c32
Meng Xiangzhuo <aumo@foxmail.com>
2026-06-01 03:13:49 +0800

    .

--- install.sh
+++ install.sh
@@ -1,27 +1,4 @@
 #!/usr/bin/bash
 
-if [[ $(uname) == "Darwin" ]]; then
-  brew bundle
-else
-  package_list=(
-    stow
-    btop
-    gh
-    bat
-    eza
-    fzf
-    starship
-    tmux
-    zoxide
-    zsh-autosuggestions
-    zsh-syntax-highlighting
-
-    # neovim stuffs
-    neovim
-    lazygit
-  )
-  sudo apt update
-  sudo apt install -y "${package_list[@]}"
-fi
-
+brew bundle
 stow home
